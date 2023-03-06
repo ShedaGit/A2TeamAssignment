3bd40594115d5c7618e42f67f171b6e04215dacd
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-03-06 10:02:25 +0300

    refactor, extract http client logic to a separate class

---
+++ A2TeamAssignment/HttpClientManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2TeamAssignment
+{
+    public class HttpClientManager : IDisposable
+    {
+        private HttpClient _httpClient;
+
+        public HttpClientManager()
+        {
+            _httpClient = new HttpClient
+            {
+                Timeout = TimeSpan.FromSeconds(10),
+                BaseAddress = new Uri("https://www.lesegais.ru")
+            };
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.9,ru;q=0.8,ja;q=0.7,hu;q=0.6,uk;q=0.5,de;q=0.4");
+            _httpClient.DefaultRequestHeaders.Add("Origin", "https://www.lesegais.ru");
+            _httpClient.DefaultRequestHeaders.Add("Referer", "https://www.lesegais.ru/open-area/deal");
+            _httpClient.DefaultRequestHeaders.Add("sec-ch-ua", "Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"");
+            _httpClient.DefaultRequestHeaders.Add("sec-ch-ua-mobile", "?0");
+            _httpClient.DefaultRequestHeaders.Add("sec-ch-ua-platform", "\"Windows\"");
+        }
+
+        public void Dispose()
+        {
+            _httpClient.Dispose();
+        }
+
+        public async Task<string> GetDealsAsync(string query)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://www.lesegais.ru/open-area/graphql"),
+                Headers =
+                {
+                    { "Connection", "keep-alive" },
+                    { "Sec-Fetch-Dest", "empty" },
+                    { "Sec-Fetch-Mode", "cors" },
+                    { "Sec-Fetch-Site", "same-origin" },
+                    { "User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36" },
+                },
+                Content = new StringContent(query, Encoding.UTF8, "application/json")
+            };
+
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
--- A2TeamAssignment/Program.cs
+++ A2TeamAssignment/Program.cs
@@ -22,43 +22,15 @@ namespace A2TeamAssignment
             var insertQuery = appSettings["insertQuery"];
             var queryWoodDealContent = appSettings["searchReportWoodDeal"];
 
-            using (var httpClient = new HttpClient())
+            using (var httpClientManager = new HttpClientManager())
             {
-                httpClient.Timeout = TimeSpan.FromSeconds(10);
+                var body = await httpClientManager.GetDealsAsync(queryWoodDealContent);
 
                 while (pageNumber < totalPages)
                 {
-                    var request = new HttpRequestMessage
-                    {
-                        Method = HttpMethod.Post,
-                        RequestUri = new Uri("https://www.lesegais.ru/open-area/graphql"),
-                        Headers =
-                        {
-                            { "Accept", "*/*" },
-                            { "Accept-Language", "en-US,en;q=0.9,ru;q=0.8,ja;q=0.7,hu;q=0.6,uk;q=0.5,de;q=0.4" },
-                            { "Connection", "keep-alive" },
-                            { "Origin", "https://www.lesegais.ru" },
-                            { "Referer", "https://www.lesegais.ru/open-area/deal" },
-                            { "Sec-Fetch-Dest", "empty" },
-                            { "Sec-Fetch-Mode", "cors" },
-                            { "Sec-Fetch-Site", "same-origin" },
-                            { "User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36" },
-                            { "sec-ch-ua", "Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"" },
-                            { "sec-ch-ua-mobile", "?0" },
-                            { "sec-ch-ua-platform", "\"Windows\"" },
-                        },
-                        Content = new StringContent(queryWoodDealContent, Encoding.UTF8, "application/json")
-                    };
-
-
                     using (var dbManager = new DatabaseManager(appSettings["databaseConnectionString"]))
                     using (SqlCommand command = new SqlCommand(insertQuery, dbManager.GetConnection()))
-                    using (var response = await httpClient.SendAsync(request)) 
-
                     {
-                        response.EnsureSuccessStatusCode();
-                        var body = await response.Content.ReadAsStringAsync();
-
                         Root deals = JsonConvert.DeserializeObject<Root>(body);
 
                         if (pageNumber == 0)
