87cb4fc5f2da8d97927ed07efc1716287021988e
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-03-06 08:45:05 +0300

    add WoodDealsData class for deserializing
    The data is created based on the API endpoint:
    https://www.lesegais.ru/open-area/graphql

---
+++ A2TeamAssignment/WoodDealsData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2TeamAssignment
+{
+    public class Content
+    {
+        public string sellerName { get; set; }
+        public string sellerInn { get; set; }
+        public string buyerName { get; set; }
+        public string buyerInn { get; set; }
+        public string woodVolumeBuyer { get; set; }
+        public string woodVolumeSeller { get; set; }
+        public string dealDate { get; set; }
+        public string dealNumber { get; set; }
+        public string __typename { get; set; }
+    }
+
+    public class Data
+    {
+        public SearchReportWoodDeal searchReportWoodDeal { get; set; }
+    }
+
+    public class Root
+    {
+        public Data data { get; set; }
+    }
+
+    public class SearchReportWoodDeal
+    {
+        public List<Content> content { get; set; }
+        public int total { get; set; }
+        public int number { get; set; }
+        public int size { get; set; }
+        public string __typename { get; set; }
+    }
+}
