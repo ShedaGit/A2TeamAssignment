021e29aeccf189ff37307a0d022fa6171e60fcda
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-03-06 10:48:03 +0300

    add timer to process deal every 10 min

--- A2TeamAssignment/Program.cs
+++ A2TeamAssignment/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Configuration;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace A2TeamAssignment
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             var appSettings = ConfigurationManager.AppSettings;
             var insertQuery = appSettings["insertQuery"];
@@ -15,6 +14,15 @@ namespace A2TeamAssignment
 
             var dealProcessor = new WoodDealsProcessor(queryWoodDealContent, insertQuery);
 
+            var timer = new Timer(ParseWoodDeals, dealProcessor, TimeSpan.Zero, TimeSpan.FromMinutes(10));
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static async void ParseWoodDeals(object state)
+        {
+            var dealProcessor = (WoodDealsProcessor)state;
             await dealProcessor.ProcessDealsAsync();
         }
     }
