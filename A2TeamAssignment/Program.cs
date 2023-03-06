a9ec86e9e1e52340fa1cee322d719934ae69589d
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-03-06 09:53:47 +0300

    refactor validation logic in main program

--- A2TeamAssignment/Program.cs
+++ A2TeamAssignment/Program.cs
@@ -2,14 +2,11 @@
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using System.Data.SqlClient;
-using System.IO;
 using System.Data;
-using System.Threading;
-using System.Text.RegularExpressions;
 using System.Configuration;
+using System.Collections.Generic;
 
 namespace A2TeamAssignment
 {
@@ -70,95 +67,34 @@ namespace A2TeamAssignment
                             totalPages = (int)Math.Ceiling((double)total / pageSize);
                         }
 
+                        var parameters = new List<SqlParameter>
+                        {
+                            new SqlParameter("@SellerName", SqlDbType.NVarChar),
+                            new SqlParameter("@SellerInn", SqlDbType.Char),
+                            new SqlParameter("@BuyerName", SqlDbType.NVarChar),
+                            new SqlParameter("@BuyerInn", SqlDbType.Char),
+                            new SqlParameter("@WoodVolumeBuyer", SqlDbType.Float),
+                            new SqlParameter("@WoodVolumeSeller", SqlDbType.Float),
+                            new SqlParameter("@DealDate", SqlDbType.DateTime),
+                            new SqlParameter("@DealNumber", SqlDbType.NVarChar)
+                        };
 
-                        var sellerNameParam = new SqlParameter("@SellerName", SqlDbType.NVarChar);
-                        var sellerInnParam = new SqlParameter("@SellerInn", SqlDbType.Char);
-                        var buyerNameParam = new SqlParameter("@BuyerName", SqlDbType.NVarChar);
-                        var buyerInnParam = new SqlParameter("@BuyerInn", SqlDbType.Char);
-                        var woodVolumeBuyerParam = new SqlParameter("@WoodVolumeBuyer", SqlDbType.Float);
-                        var woodVolumeSellerParam = new SqlParameter("@WoodVolumeSeller", SqlDbType.Float);
-                        var dealDateParam = new SqlParameter("@DealDate", SqlDbType.DateTime);
-                        var dealNumberParam = new SqlParameter("@DealNumber", SqlDbType.NVarChar);
-
-                        // add parameters to command
-                        command.Parameters.Add(sellerNameParam);
-                        command.Parameters.Add(sellerInnParam);
-                        command.Parameters.Add(buyerNameParam);
-                        command.Parameters.Add(buyerInnParam);
-                        command.Parameters.Add(woodVolumeBuyerParam);
-                        command.Parameters.Add(woodVolumeSellerParam);
-                        command.Parameters.Add(dealDateParam);
-                        command.Parameters.Add(dealNumberParam);
+                        command.Parameters.AddRange(parameters.ToArray());
 
                         foreach (var deal in deals.data.searchReportWoodDeal.content)
                         {
+                            var validator = new WoodDealsValidator();
 
-                            if (IsValidName(deal.sellerName))
+                            if (validator.IsValidDeal(deal, parameters))
                             {
-                                sellerNameParam.Value = deal.sellerName;
-                            }
-                            else
-                            {
-                                continue;
-                            }
-
-                            if (IsValidInn(deal.sellerInn))
-                            {
-                                sellerInnParam.Value = deal.sellerInn;
-                            }
-                            else
-                            {
-                                continue;
-                            }
-
-                            if (IsValidName(deal.buyerName))
-                            {
-                                buyerNameParam.Value = deal.buyerName;
-                            }
-                            else
-                            {
-                                continue;
-                            }
-
-                            if (IsValidInn(deal.sellerInn))
-                            {
-                                buyerInnParam.Value = deal.buyerInn;
-                            }
-                            else
-                            {
-                                continue;
-                            }
-
-                            if (IsValidDouble(deal.woodVolumeBuyer))
-                            {
-                                woodVolumeBuyerParam.Value = Math.Round(Double.Parse(deal.woodVolumeBuyer), 2);
-                            }
-                            else
-                            {
-                                continue;
-                            }
-
-                            if (IsValidDouble(deal.woodVolumeSeller))
-                            {
-                                woodVolumeSellerParam.Value = Math.Round(Double.Parse(deal.woodVolumeSeller), 2);
-                            }
-                            else
-                            {
-                                continue;
-                            }
-
-                            if (IsValidDate(deal.dealDate))
-                            {
-                                dealDateParam.Value = deal.dealDate;
-                            }
-                            else
-                            {
-                                continue;
-                            }
-
-                            if (IsValidNumeric(deal.dealNumber))
-                            {
-                                dealNumberParam.Value = deal.dealNumber;
+                                parameters[0].Value = deal.sellerName;
+                                parameters[1].Value = deal.sellerInn;
+                                parameters[2].Value = deal.buyerName;
+                                parameters[3].Value = deal.buyerInn;
+                                parameters[4].Value = Math.Round(Double.Parse(deal.woodVolumeBuyer), 2);
+                                parameters[5].Value = Math.Round(Double.Parse(deal.woodVolumeSeller), 2);
+                                parameters[6].Value = deal.dealDate;
+                                parameters[7].Value = deal.dealNumber;
                             }
                             else
                             {
@@ -166,15 +102,6 @@ namespace A2TeamAssignment
                             }
 
                             int rowsAffected = command.ExecuteNonQuery();
-
-                            if (rowsAffected > 0)
-                            {
-                                Console.WriteLine("The deal was inserted successfully.");
-                            }
-                            else
-                            {
-                                Console.WriteLine("The deal already exists in the database.");
-                            }
                         }
                     }
 
@@ -184,87 +111,6 @@ namespace A2TeamAssignment
                     await Task.Delay(TimeSpan.FromSeconds(10));
                 }
             }
-        }
-
-
-        public static bool IsValidName(string inputName)
-        {
-            return !string.IsNullOrEmpty(inputName) && inputName.Length <= 255;
-        }
-
-        public static bool IsValidInn(string inn)
-        {
-            if (inn.Length == 10)
-            {
-                int[] multipliers = { 2, 4, 10, 3, 5, 9, 4, 6, 8 };
-                int sum = 0;
-
-                for (int i = 0; i < 9; i++)
-                {
-                    sum += int.Parse(inn[i].ToString()) * multipliers[i];
-                }
-
-                int control = sum % 11;
-                control = (control == 10) ? 0 : control;
-
-                return control == int.Parse(inn[9].ToString());
-            }
-            else if (inn.Length == 12)
-            {
-                int[] multipliers1 = { 7, 2, 4, 10, 3, 5, 9, 4, 6, 8 };
-                int[] multipliers2 = { 3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8 };
-
-                int sum1 = 0;
-                for (int i = 0; i < 10; i++)
-                {
-                    sum1 += int.Parse(inn[i].ToString()) * multipliers1[i];
-                }
-
-                int control1 = sum1 % 11;
-                control1 = (control1 == 10) ? 0 : control1;
-
-                if (control1 != int.Parse(inn[10].ToString()))
-                {
-                    return false;
-                }
-
-                int sum2 = 0;
-                for (int i = 0; i < 11; i++)
-                {
-                    sum2 += int.Parse(inn[i].ToString()) * multipliers2[i];
-                }
-
-                int control2 = sum2 % 11;
-                control2 = (control2 == 10) ? 0 : control2;
-
-                return control2 == int.Parse(inn[11].ToString());
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        private static bool IsValidDouble(string woodVolume)
-        {
-            return Regex.IsMatch(woodVolume, @"^\d+(\.\d+)?$");
-        }
-
-        private static bool IsValidDate(string dealDate)
-        {
-            if (Regex.IsMatch(dealDate, @"^(19|20)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$"))
-            {
-                return DateTime.Parse(dealDate) <= DateTime.Now;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        private static bool IsValidNumeric(string dealNumber)
-        {
-            return Regex.IsMatch(dealNumber, @"^[0-9]+$");
         }
     }
 }
