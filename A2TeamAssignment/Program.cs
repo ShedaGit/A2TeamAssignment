a7596261ce4a68d25a70e8acef64699d569aa4e5
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-03-06 09:36:15 +0300

    add methods for wood deals data validation

--- A2TeamAssignment/Program.cs
+++ A2TeamAssignment/Program.cs
@@ -60,8 +60,6 @@ namespace A2TeamAssignment
                         response.EnsureSuccessStatusCode();
                         var body = await response.Content.ReadAsStringAsync();
 
-                        Console.WriteLine(body);
-
                         Root deals = JsonConvert.DeserializeObject<Root>(body);
 
                         if (pageNumber == 0)
@@ -77,6 +75,87 @@ namespace A2TeamAssignment
                     await Task.Delay(TimeSpan.FromSeconds(10));
                 }
             }
+        }
+
+
+        public static bool IsValidName(string inputName)
+        {
+            return !string.IsNullOrEmpty(inputName) && inputName.Length <= 255;
+        }
+
+        public static bool IsValidInn(string inn)
+        {
+            if (inn.Length == 10)
+            {
+                int[] multipliers = { 2, 4, 10, 3, 5, 9, 4, 6, 8 };
+                int sum = 0;
+
+                for (int i = 0; i < 9; i++)
+                {
+                    sum += int.Parse(inn[i].ToString()) * multipliers[i];
+                }
+
+                int control = sum % 11;
+                control = (control == 10) ? 0 : control;
+
+                return control == int.Parse(inn[9].ToString());
+            }
+            else if (inn.Length == 12)
+            {
+                int[] multipliers1 = { 7, 2, 4, 10, 3, 5, 9, 4, 6, 8 };
+                int[] multipliers2 = { 3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8 };
+
+                int sum1 = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    sum1 += int.Parse(inn[i].ToString()) * multipliers1[i];
+                }
+
+                int control1 = sum1 % 11;
+                control1 = (control1 == 10) ? 0 : control1;
+
+                if (control1 != int.Parse(inn[10].ToString()))
+                {
+                    return false;
+                }
+
+                int sum2 = 0;
+                for (int i = 0; i < 11; i++)
+                {
+                    sum2 += int.Parse(inn[i].ToString()) * multipliers2[i];
+                }
+
+                int control2 = sum2 % 11;
+                control2 = (control2 == 10) ? 0 : control2;
+
+                return control2 == int.Parse(inn[11].ToString());
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static bool IsValidDouble(string woodVolume)
+        {
+            return Regex.IsMatch(woodVolume, @"^\d+(\.\d+)?$");
+        }
+
+        private static bool IsValidDate(string dealDate)
+        {
+            if (Regex.IsMatch(dealDate, @"^(19|20)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$"))
+            {
+                return DateTime.Parse(dealDate) <= DateTime.Now;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static bool IsValidNumeric(string dealNumber)
+        {
+            return Regex.IsMatch(dealNumber, @"^[0-9]+$");
         }
     }
 }
